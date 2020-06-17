e793705d45f8576de6782453ccda59e843009478
syh1011 <syh@hbilab.org>
2020-06-17 15:57:32 +0900

    2020-06-17-v6

--- WebContent/member/checkLogin.jsp
+++ WebContent/member/checkLogin.jsp
@@ -1,8 +1,26 @@
+<%@page import="kr.or.kpc.dto.CustomerDto"%>
+<%@page import="kr.or.kpc.dao.CustomerDao"%>
 <%@ page language="java" pageEncoding="UTF-8"%>
 <%
 	String email = request.getParameter("email");
 	String pwd = request.getParameter("pwd");
 	
+	CustomerDao dao = CustomerDao.getInstance();
+	CustomerDto dto = dao.getLogin(email, pwd);
 	
+	if(dto != null){
+		//email, pwd가 일치하는 경우
+		//세션에 클라이언트 정보를 저장한다.
+		session.setAttribute("login",dto);
+		response.sendRedirect("/notice/list.jsp?page=1");
+	}else{
+		//email, pwd가 일치하지 않은 경우
+		%>
+		<script>
+			alert("이메일 혹은 비밀번호가 일치하지 않습니다.");
+			history.back(-1);
+		</script>
+		<%
+	}
 	
 %>
---
+++ src/kr/or/kpc/dto/CustomerDto.java
@@ -0,0 +1,37 @@
+package kr.or.kpc.dto;
+
+public class CustomerDto {
+	private String email;
+	private String pwd;
+	private String name;
+	public CustomerDto() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public CustomerDto(String email, String pwd, String name) {
+		super();
+		this.email = email;
+		this.pwd = pwd;
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
