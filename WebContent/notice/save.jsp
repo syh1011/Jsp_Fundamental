69664d0d2b8a5ad08802354e8b60d58f96a653e8
syh1011 <syh@hbilab.org>
2020-06-17 10:55:23 +0900

    2020-06-17-v2

--- WebContent/notice/save.jsp
+++ WebContent/notice/save.jsp
@@ -1,9 +1,32 @@
+<%@page import="kr.or.kpc.dto.NoticeDto"%>
+<%@page import="kr.or.kpc.dao.NoticeDao"%>
 <%@ page language="java" pageEncoding="UTF-8"%>
 <%
 	request.setCharacterEncoding("utf-8");
 	String writer = request.getParameter("writer");
 	String title = request.getParameter("title");
 	String content = request.getParameter("content");
+	int num = 0;
+	NoticeDao dao = NoticeDao.getInstance();
+	num = dao.getMaxNum();
+	NoticeDto dto = new NoticeDto(num, writer,title,content,null);
+	
+	int resultCount = dao.insert(dto);
+	if(resultCount == 1){
+		%>
+		<script>
+			alert('글이 추가 되었습니다.');
+			location.href="list.jsp?page=1";
+		</script>
+		<%
+	}else{
+		%>
+		<script>
+			alert('에러..');
+			history.back(-1);
+		</script>
+		<%
+	}
 %>
 
 
--- src/kr/or/kpc/dao/NoticeDao.java
+++ src/kr/or/kpc/dao/NoticeDao.java
@@ -265,6 +265,42 @@ public class NoticeDao {
 		return count;
 	}
 	
+	public int getMaxNum() {
+		int max = 0 ;
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			con = ConnLocator.getConnect();
+			StringBuffer sql = new StringBuffer();
+			sql.append("SELECT ifnull(MAX(n_num)+1,1) FROM notice");
+			
+			pstmt = con.prepareStatement(sql.toString());
+			
+			int index = 0;
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				index = 0;
+				max = rs.getInt(++index);
+			}
+			
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} finally {
+			try {
+				if(rs != null) rs.close();
+				if(pstmt != null) pstmt.close();
+				if(con != null) con.close();
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			
+		}
+		return max;
+	}
+	
 	
 	
 	
